aa4c38d3b6920bc024b9881315120f8b1ae95758
level1 <yash8864919763@gmail.com>
2021-05-31 08:22:44 +0530

    first version

---
+++ Home/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+     
+admin.site.site_header="prema life sciences"
+admin.site.site_title="welcome to our Dashboard"
+admin.site.index_title="welcome to this portal"
+urlpatterns = [
+     path('',views.Home, name='Home'),
+     path('AboutUS',views.AboutUS, name='AboutUS'),
+     path('Contact',views.Contact, name='Contact'),
+     path('products/<str:slug>',views.PRODUCTS, name='PRODUCTS'),
+     path('products',views.PRODUCTSHOME, name='PRODUCTSHOME'),
+     path('covid',views.COVID19SOLUTIONSHOME, name='COVID19SOLUTIONSHOME'),
+     path('covid/<str:slug>',views.COVID19SOLUTIONS, name='COVID19SOLUTIONS'),
+     path('media',views.MEDIA, name='MEDIA'),
+     path('career',views.CAREER, name='CAREER')
+]
